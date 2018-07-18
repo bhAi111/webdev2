20a10b7c4dcb9a29a5f900eb4c202351c582eaed
Jessie <jessieconcepcion1234@gmail.com>
2018-07-18 23:17:52 +0800

    remove shitty javascript
    Signed-off-by: Jessie <jessieconcepcion1234@gmail.com>

--- footer.php
+++ footer.php
@@ -1,15 +1,5 @@
 
-  <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
-  <script src="js/bootstrap.js"></script>
-  <script type="text/javascript">
-      $(window).on('scroll', function() {
-          if ($(window).scrollTop()) {
-              $('nav').addStye('black');
-          } else {
-              $('nav').removeClass('black');
-          }
-      })
-  </script>
+<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+<script src="js/bootstrap.js"></script>
 </body>
-
 </html>
