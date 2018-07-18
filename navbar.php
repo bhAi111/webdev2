bef2338a52c7496842287a2a374289d0d2d9a588
Jessie <jessieconcepcion1234@gmail.com>
2018-07-18 23:20:04 +0800

    improved navbar with icons
    Signed-off-by: Jessie <jessieconcepcion1234@gmail.com>

--- navbar.php
+++ navbar.php
@@ -9,16 +9,32 @@
                 <span class="icon-bar"></span>
             </button>
             <a class="navbar-brand" href="index.php">
-                  <img style="max-width:100px; margin-top: -7px;" src="img/logo.png">
+                  <img src="img/logo.png">
+                  <span>GG SHOP</span>
             </a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li><a href="products.php">Products</a></li>
-                <li><a href="about-us.php">About Us</a></li>
-                <li><a href="contact-us.php">Contact Us</a></li>
+                <li>
+                    <a href="products.php">
+                        <i class="fa fa-product-hunt"></i>
+                        <span>Products</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="about-us.php">
+                        <i class="fa fa-question-circle"></i>
+                        <span>About Us</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="contact-us.php">
+                        <i class="fa fa-phone"></i>
+                        <span>Contact Us</span>
+                    </a>
+                </li>
                 
             </ul>
             <ul class="nav navbar-nav navbar-right">
