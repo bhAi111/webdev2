8ace4aba85927edb8b350929520b062c6b38be22
Jessie <jessieconcepcion1234@gmail.com>
2018-07-18 23:32:34 +0800

    contact us redesign
    Signed-off-by: Jessie <jessieconcepcion1234@gmail.com>

--- contact-us.php
+++ contact-us.php
@@ -2,14 +2,26 @@
 <?php include_once('navbar.php'); ?>
 <div class="wrapper">
 
-  <section class="sec1"></section>
-  <section class="content">
-      <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
-      <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
-  </section>
+	<div class="container">
+		<form action="#" method="post">
+	  		<div class="form-group">
+			    <label for="firstname">Fullname</label>
+			    <input type="text" class="form-control" name="firstname" placeholder="Email">
+		  	</div>
+		  	<div class="form-group">
+			    <label for="email">Email address</label>
+			    <input type="email" class="form-control" name="email" placeholder="Email">
+		  	</div>
+		  	<div class="form-group">
+			    <label for="subject">Subject</label>
+			    <input type="text" class="form-control" name="subject" placeholder="Email">
+		  	</div>
+		  	<div class="form-group">
+			    <label for="subject">Subject</label>
+			    <textarea class="form-control" name="message" placeholder="Write your message here"></textarea>
+		  	</div>
+		  	<button type="submit" class="btn btn-default">Submit</button>
+		</form>
+	</div>
 </div>
 <?php include_once('footer.php'); ?>
